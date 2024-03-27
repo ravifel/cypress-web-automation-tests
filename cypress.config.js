8f8b7039268db69e5ec1938009fc2601399a567b
Ravi Silva <ravifelipee@gmail.com>
2024-03-27 20:56:55 -0300

    adition viewport

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 10000,
+    viewportWidth: 335,
+    viewportHeight: 999,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
